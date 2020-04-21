9974748edcf37cceeab149a2c41b1f1727354a11
marcelojrfarias <marcelojrfarias@outlook.com>
2020-04-20 21:00:01 -0300

    Improves the application style and adds a readme and license file.

---
+++ README.md
@@ -0,0 +1,85 @@
+<h1 align="center">
+    <img alt="GoStack Bootcamp" src="https://res.cloudinary.com/marcelojrfarias/image/upload/v1587323057/gostack_gy3h7u.png" />
+    <br>
+    Challenge #2
+    <br>
+    ReactJS Concepts
+</h1>
+
+<h4 align="center">
+  An WEB Application to create and delete repositories.
+</h4>
+<p align="center">
+  <img alt="GitHub top language" src="https://img.shields.io/github/languages/top/marcelojrfarias/gostack-reactjs_concepts.svg">
+  
+  <img alt="GitHub language count" src="https://img.shields.io/github/languages/count/marcelojrfarias/gostack-reactjs_concepts.svg">
+  
+  <a href="https://www.codacy.com/manual/marcelojrfarias/gostack-reactjs_concepts?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=marcelojrfarias/gostack-reactjs_concepts&amp;utm_campaign=Badge_Grade">
+    <img src="https://api.codacy.com/project/badge/Grade/c65d126792ea4aa9a5f4139b8bc708d5"/>
+  </a>
+  
+  <img alt="Repository size" src="https://img.shields.io/github/repo-size/marcelojrfarias/gostack-reactjs_concepts.svg">
+  <a href="https://github.com/marcelojrfarias/gostack-reactjs_concepts/commits/master">
+    <img alt="GitHub last commit" src="https://img.shields.io/github/last-commit/marcelojrfarias/gostack-reactjs_concepts.svg">
+  </a>
+  
+  <a href="https://github.com/marcelojrfarias/gostack-reactjs_concepts/issues">
+    <img alt="Repository issues" src="https://img.shields.io/github/issues/marcelojrfarias/gostack-reactjs_concepts.svg">
+  </a>
+  
+  <img alt="GitHub" src="https://img.shields.io/github/license/marcelojrfarias/gostack-reactjs_concepts.svg"> 
+</p>
+
+<p align="center">
+  <a href="#rocket-technologies">Technologies</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#gear-how-to-use">How To Use</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#computer-web-application">Application</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-license">License</a>
+</p>
+
+## :rocket: Technologies
+
+This project was developed at the [RocketSeat GoStack Bootcamp][gostack] with the following technologies:
+
+- [ReactJS][reactjs]
+- [NodeJS][nodejs]
+- [Axios][axios]
+- [Visual Studio Code][vscode]
+- And some other packages...
+  
+## :gear: How To Use
+
+To clone and run this application, you'll need [Git][git], [Node.js v10.16][nodejs] or higher + [Yarn v1.13][yarn] or higher installed on your computer. From your command line:
+
+```bash
+# Clone this repository
+$ git clone https://github.com/marcelojrfarias/gostack-reactjs_concepts
+
+# Go into the repository
+$ cd gostack-reactjs_concepts
+
+# Install dependencies
+$ yarn install
+
+# Run the app
+$ yarn start
+```
+
+## :computer: Application
+  <img align="center" alt="App Screenshot" src="https://res.cloudinary.com/marcelojrfarias/image/upload/v1587426888/app-screenshot_kotoou.png"/>
+
+## :memo: License
+This project is under the MIT license. See the [LICENSE](https://github.com/marcelojrfarias/gostack-reactjs_concepts/blob/master/LICENSE) for more information.
+
+---
+
+Made with 💗 by Marcelo Farias 👋 [Get in touch!](https://www.linkedin.com/in/marcelojrfarias/)
+
+[nodejs]: https://nodejs.org/
+[gostack]: https://rocketseat.com.br/bootcamp
+[express]: https://expressjs.com/
+[git]: https://git-scm.com
+[yarn]: https://yarnpkg.com/
+[vscode]: https://code.visualstudio.com/
+[axios]: https://github.com/axios/axios
+[reactjs]: https://reactjs.org/
--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import api from './services/api'
 
 import "./styles.css";
 
+import logo from './assets/gostack.svg'
+
 function App() {
 
   const [ repositories, setRepositories ] = useState([])
+  const [ repository, setRepository ] = useState({title: '', url: '', techs: ['']})
+  // const [ title, setTitle ] = useState([])
+  // const [ url, setURL ] = useState([])
+  // const [ techs, setTechs ] = useState([])
+  
 
   useEffect(() => {
     api.get('/repositories').then(response => {
@@ -13,16 +20,18 @@ function App() {
     })
   }, [])
 
-  async function handleAddRepository() {
-    const repository = {
-      "url": "https://github.com/marcelojrfarias/",
-      "title": `Repository ${Date.now()}`,
-      "techs": ["NodeJS", "ReactJS", "ReactNative"]
-    }
+  async function handleAddRepository(repository) {
+    // const repository = {
+    //   "url": "https://github.com/marcelojrfarias/",
+    //   "title": `Repository ${Date.now()}`,
+    //   "techs": ["NodeJS", "ReactJS", "ReactNative"]
+    // }
 
     const response = await api.post('/repositories', repository)
 
     setRepositories([...repositories, response.data])
+
+    setRepository({title: '', url: '', techs: ['']})
   }
 
   async function handleRemoveRepository(id) {
@@ -35,20 +44,85 @@ function App() {
     }
   }
 
+  async function handleSubmit(event) {
+    event.preventDefault()
+
+    if (repository.title === '')
+      return
+      
+    if (repository.url === '')
+      return
+
+    if (repository.techs === [''])
+      return
+
+    handleAddRepository(repository)
+  }
+
   return (
-    <div>
-      <h2>Repositories</h2>
-      <button onClick={handleAddRepository}>Adicionar</button>
-      <ul data-testid="repository-list">
-        {repositories.map(repository => (
-        <li key={repository.id}>
-          {repository.title}
-          <button onClick={() => handleRemoveRepository(repository.id)}>
-            Remover
-          </button>
-        </li>
-        ))}
-      </ul>
+    <div className="container">
+
+      <img src={logo} alt="GoStack"/>
+      
+      <div className="content">
+        
+        <p className="slogan">
+          Adicione seus <strong>repositórios</strong> ao seu <strong>portfólio</strong>.
+        </p>
+
+        {/* onClick={handleAddRepository} */}
+        <form onSubmit={handleSubmit}>
+
+          <label htmlFor="title">TÍTULO:*</label>
+          <input 
+            id="title"
+            type="text"
+            className="title"
+            placeholder="Título do seu projeto"
+            value={repository.title}
+            onChange = {event => setRepository({...repository, title: event.target.value})}
+          />
+
+          <label htmlFor="url">LINK:*</label>
+          <input 
+            id="url"
+            type="url"
+            className="url"
+            placeholder="Link para o seu projeto"
+            value={repository.url}
+            onChange = {event => setRepository({...repository, url: event.target.value})}
+          />
+
+          <label htmlFor="techs">TECNOLOGIAS:*</label>
+          <input 
+            id="techs"
+            type="text"
+            className="techs"
+            placeholder="Tecnologias utilizadas no seu projeto"
+            value={repository.techs}
+            onChange = {event => setRepository({...repository, techs: event.target.value.split(',')})}
+          />
+
+          <button type="submit">Adicionar</button>
+        </form>
+
+      </div>
+        
+      {repositories.length > 0 && (<div className="list">
+        <p className="slogan">
+          <strong>Repositórios:</strong>
+        </p>
+        <ul data-testid="repository-list">
+          {repositories.map(repository => (
+          <li key={repository.id}>
+            <a href={repository.url}>{repository.title}</a>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+          ))}
+        </ul>
+      </div>)}
 
     </div>
   );
